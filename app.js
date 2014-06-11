60adf79d69a7209acfe377041a137b51fed355f8
Patrick Ding <patrickding@Patricks-MacBook-Pro-3.local>
2014-06-10 23:06:58 -0400

    set up

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ var express 	= require('express');
 var routes  	= require('./routes');
 var http 		= require('http');
 var path		= require('path');
+var serveStatic = require('serve-static');
+var fs 			= require('fs');
 
 var app = express();
 
-app.get('/', routes.index);
-
+// mount your routes...
+app.use(express.static(path.join(__dirname + '/public')));
+// after static
+app.use('/', routes.index);
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, 'public')));
 http.createServer(app).listen(app.get('port'), function(){
  	console.log('Express server listening on port ' + app.get('port'));
+ 	fs.readdir(__dirname + '/public', function(err, files) {
+ 		console.log(files);
+ 	});
 });
