67aff8c74e601e988dfda361931c6126f657a13a
Patrick Ding <patrickding@172-26-199-216.dynapool.nyu.edu>
2014-06-13 22:02:50 -0400

    working routes

--- app.js
+++ app.js
@@ -10,13 +10,20 @@ var app = express();
 // mount your routes...
 app.use(express.static(path.join(__dirname + '/public')));
 // after static
-app.use('/', routes.index);
+
+// mount routes
+app.get('/', routes.index);
+app.get('/about', routes.about);
+app.get('/people', routes.people);
+app.get('/photos', routes.photos);
+app.get('/calendar', routes.calendar);
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 http.createServer(app).listen(app.get('port'), function(){
  	console.log('Express server listening on port ' + app.get('port'));
- 	fs.readdir(__dirname + '/public', function(err, files) {
+ 	fs.readdir(__dirname + '/routes', function(err, files) {
  		console.log(files);
  	});
 });
--- routes/index.js
+++ routes/index.js
@@ -4,3 +4,31 @@ exports.index = function(req, res) {
 		title: 'Princeton Chess Club'
 	});
 };
+
+exports.about = function(req, res) {
+	console.log(req.path);
+	res.render('about', {
+		title: 'About'
+	});
+}; 
+
+exports.people = function(req, res) {
+	console.log(req.path);
+	res.render('people', {
+		title: 'People'
+	});
+};
+
+exports.photos = function(req, res) {
+	console.log(req.path);
+	res.render('photos', {
+		title: 'Photos'
+	});
+};
+
+exports.calendar = function(req, res) {
+	console.log(req.path);
+	res.render('calendar', {
+		title: 'Calendar'
+	});
+};
